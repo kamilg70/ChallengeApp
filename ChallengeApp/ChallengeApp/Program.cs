ec5067ea503a2c4c68d901ceb8849eb737b2365b
Kamil Glapiński <kgl.1996@wp.pl>
2024-04-09 11:51:05 +0200

    czwarteZadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ 
+string name = "Ewa";
+var age = 30;
+Gender gender = Gender.Female;
+
+if (gender == Gender.Female && age < 30)
+{
+Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+Console.WriteLine("Ewa, lat 30");
+}
+else if (gender == Gender.Male && age < 18)
+{
+Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+enum Gender
+{
+    Male,
+    Female
+}
